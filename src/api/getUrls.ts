dd50d1e5137c640608da3435b6cdfbf87ac69f8e
Borja Cuerva <borjacuerva@gmail.com>
2024-06-17 00:06:58 +0200

    chore(api): Add getAllUrls function for fetching URLs

---
+++ src/api/getUrls.ts
@@ -0,0 +1,20 @@
+import { ref } from 'vue'
+
+type Urls = Array<{
+  id: number
+  name: string
+  description: string
+  slug: string
+}>
+
+export function getAllUrls(url: string) {
+  const data = ref<Urls>([])
+  const error = ref(null)
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((response) => (data.value = response))
+    .catch((err) => (error.value = err))
+
+  return { data, error }
+}
