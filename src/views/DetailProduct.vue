6077954bd5859fdeb180cb1bd8ec4a760b3b5242
Borja Cuerva <borjacuerva@gmail.com>
2024-06-17 20:55:25 +0200

    chore(view): Add DetailProduct view component for displaying product details

---
+++ src/views/DetailProduct.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const props = defineProps({
+  name: String,
+  description: String,
+  price: Number,
+  category: String,
+  stock: Number,
+  image_url: String,
+  url: String,
+  hola: Object
+})
+
+const route = useRoute()
+
+console.log(props.hola)
+
+console.log(route)
+</script>
+<template>
+  <div>
+    <h1>Product Detail</h1>
+
+    <p>{{ route.params }}</p>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
